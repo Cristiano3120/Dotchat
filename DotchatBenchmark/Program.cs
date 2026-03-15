66242582e2eac7c99130076b6d8d4d90712fdea7
Cristiano3120 <Cristianocx7@gmail.com>
2026-03-15 22:29:47 +0100

    Add project files.

---
+++ DotchatBenchmark/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DotchatBenchmark;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+    }
+}
